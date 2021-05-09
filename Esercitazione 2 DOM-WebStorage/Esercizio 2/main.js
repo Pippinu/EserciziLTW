b8d59f8bdd8d5c0a7d380079744aef312c403409
Pippinu <iacono760@gmail.com>
2021-05-09 12:27:07 +0200

    1.3

--- Esercitazione 2 DOM-WebStorage/Esercizio 2/main.js
+++ Esercitazione 2 DOM-WebStorage/Esercizio 2/main.js
@@ -1,17 +1,5 @@
 window.onload = function(){
-    // let c1 = document.getElementById('c1').addEventListener('mouseenter', function(event){
-    //     console.log($('div').attr('background-color'));
-    //     console.log($('div').attr('width'));
-
-    // });
-
-    $('#c1').click(function(){                     
-        alert($('a',this).attr('id'));                    
-    });
+    let c1 = $('.c1').get(0).addEventListener('mouseenter', function(event){
+        confirm('BG: ' + JSON.parse(this.attributes));
+    })
 }
-
-// $(document).ready(function () {
-//     $('#c1').click(function(){                     
-//         alert($('a',this).attr('id'));                    
-//     });
-// });
