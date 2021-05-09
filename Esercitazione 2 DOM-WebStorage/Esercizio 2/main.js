9cf692035ee86918715dc9c9a2c034c0ebbd0f40
P1ppinu <iacono760@gmail.com>
2021-05-09 20:37:49 +0200

    Esercizio 2 Finito

--- Esercitazione 2 DOM-WebStorage/Esercizio 2/main.js
+++ Esercitazione 2 DOM-WebStorage/Esercizio 2/main.js
@@ -1,5 +1,48 @@
 window.onload = function(){
-    let c1 = $('.c1').get(0).addEventListener('mouseenter', function(event){
-        confirm('BG: ' + JSON.parse(this.attributes));
+    /* let c1 = $('.c1', '.c2').addEventListener('mouseenter', function(event){
+        confirm('BG: ' + this.attr(''));
+    }) */
+
+   /*  $('div.col').click(function(){
+        confirm('This DIV is ' + $(this).css('background-color'));
+    }) */
+
+    /* $('div.c1').mouseenter(function(){
+        $(this).attr('style', 'background-color: red');
     })
+
+    $('div.c2').mouseenter(function(){
+        $(this).attr('style', 'background-color: grey');
+    }) */
+
+    let spanC2DefaultColor = $('span.c2').css('backgroundColor');
+    
+    $('div').mouseenter(function(){
+        
+        var bgColor = $(this).css('backgroundColor');
+
+        if( bgColor === 'rgb(128, 128, 128)'){
+            $(this).attr('style',  'background-color: red');
+        } else {
+            $(this).attr('style', 'background-color: grey');
+        }
+        console.log(bgColor)
+    })
+
+    $('.c1').click(function(){
+        $(this).attr('style', 'background-color: blue');
+    })
+    $('.c1').dblclick(function(){
+        $(this).hide();
+    })
+
+    $('span.c2').mouseenter(function(){
+        $(this).attr('style',  'background-color: green');
+    })
+    $('span.c2').mouseleave(function(){
+        $(this).attr('style',  spanC2DefaultColor);
+    })
+
 }
+
+
