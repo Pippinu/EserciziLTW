0a43cac59f61434eafaee0dacb485067953dc30b
Pippinu <iacono760@gmail.com>
2021-05-10 10:14:10 +0200

    added js plain version

--- Esercitazione 2 DOM-WebStorage/Esercizio 2/main.js
+++ Esercitazione 2 DOM-WebStorage/Esercizio 2/main.js
@@ -1,48 +1,66 @@
 window.onload = function(){
-    /* let c1 = $('.c1', '.c2').addEventListener('mouseenter', function(event){
-        confirm('BG: ' + this.attr(''));
-    }) */
+    //VERSIONE JS PLAIN
+    var c1Array = document.getElementsByClassName('c1');
+    var divArray = document.getElementsByTagName('DIV');
+    var spanC2Array = document.querySelectorAll('span.c2');
 
-   /*  $('div.col').click(function(){
-        confirm('This DIV is ' + $(this).css('background-color'));
-    }) */
+    for(var i = 0; i < divArray.length; i++){
+        divArray[i].addEventListener('mouseenter', function(){
+            if(window.getComputedStyle(this, null).getPropertyValue('background-color') === 'rgb(255, 0, 0)') this.style.backgroundColor = 'grey'
+            else this.style.backgroundColor = 'red';
+        })
+    }
 
-    /* $('div.c1').mouseenter(function(){
-        $(this).attr('style', 'background-color: red');
-    })
+    for(var i = 0; i < c1Array.length; i++){
+        c1Array[i].addEventListener('click', function(){
+            this.style.backgroundColor = 'blue';
+        })
+        c1Array[i].addEventListener('dblclick', function(){
+            this.style.display = 'none';                        //nasconde e rimuove dalla pagina
+            //this.style.visibility = 'hidden';                 //nasconde soltanto
+        })
+    }
 
-    $('div.c2').mouseenter(function(){
-        $(this).attr('style', 'background-color: grey');
-    }) */
+    let spanC2DefaultColor; 
 
-    let spanC2DefaultColor = $('span.c2').css('backgroundColor');
+    Array.prototype.forEach.call(spanC2Array, el => {
+        el.addEventListener('mouseenter', function(){
+            spanC2DefaultColor = window.getComputedStyle(this, null).getPropertyValue('background-color');
+            this.style.backgroundColor = 'green'
+        })
+        el.addEventListener('mouseout', function(){
+            this.style.backgroundColor = spanC2DefaultColor;
+        })
+    });
+
+    //VERSIONE JQUERY
+    // let spanC2DefaultColor = $('span.c2').css('backgroundColor');
     
-    $('div').mouseenter(function(){
+    // $('div').mouseenter(function(){
         
-        var bgColor = $(this).css('backgroundColor');
+    // var bgColor = $(this).css('backgroundColor');
 
-        if( bgColor === 'rgb(128, 128, 128)'){
-            $(this).attr('style',  'background-color: red');
-        } else {
-            $(this).attr('style', 'background-color: grey');
-        }
-        console.log(bgColor)
-    })
+    // if( bgColor === 'rgb(128, 128, 128)'){
+    //     $(this).attr('style',  'background-color: red');
+    // } else {
+    //     $(this).attr('style', 'background-color: grey');
+    // }
+    // console.log(bgColor)
+    // })
 
-    $('.c1').click(function(){
-        $(this).attr('style', 'background-color: blue');
-    })
-    $('.c1').dblclick(function(){
-        $(this).hide();
-    })
+    // $('.c1').click(function(){
+    //     $(this).attr('style', 'background-color: blue');
+    // })
+    // $('.c1').dblclick(function(){
+    //     $(this).hide();
+    // })
 
-    $('span.c2').mouseenter(function(){
-        $(this).attr('style',  'background-color: green');
-    })
-    $('span.c2').mouseleave(function(){
-        $(this).attr('style',  spanC2DefaultColor);
-    })
-
+    // $('span.c2').mouseenter(function(){
+    //     $(this).attr('style',  'background-color: green');
+    // })
+    // $('span.c2').mouseleave(function(){
+    //     $(this).attr('style',  spanC2DefaultColor);
+    // })
 }
 
 
