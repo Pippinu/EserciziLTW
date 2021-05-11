1d876b113699cb6c1309fd0f48066e0747f2d04d
Pippinu <iacono760@gmail.com>
2021-05-11 15:32:51 +0200

    Riaggiunto Esercizio 2 Esercitazione

--- Esercitazione 2 DOM-WebStorage/Esercizio 2/main.js
+++ Esercitazione 2 DOM-WebStorage/Esercizio 2/main.js
@@ -33,11 +33,12 @@ window.onload = function(){
         })
     });
 
+    
+
     //VERSIONE JQUERY
     // let spanC2DefaultColor = $('span.c2').css('backgroundColor');
     
     // $('div').mouseenter(function(){
-        
     // var bgColor = $(this).css('backgroundColor');
 
     // if( bgColor === 'rgb(128, 128, 128)'){
@@ -60,6 +61,41 @@ window.onload = function(){
     // })
     // $('span.c2').mouseleave(function(){
     //     $(this).attr('style',  spanC2DefaultColor);
+    // })
+
+
+
+    //VERSIONE ALTERNATIVA JQUERY, notare utilizzo .on(){} [Slide 25 File JQuery]
+    //let spanC2DefaultColor = $('span.c2').css('backgroundColor');
+    
+    // $('div').mouseenter(function(){
+        
+    // var bgColor = $(this).css('backgroundColor');
+
+    // if( bgColor === 'rgb(128, 128, 128)'){
+    //     $(this).attr('style',  'background-color: red');
+    // } else {
+    //     $(this).attr('style', 'background-color: grey');
+    // }
+    // console.log(bgColor)
+    // })
+
+    // $('.c1').on({
+    //     click: function(){
+    //         $(this).attr('style', 'background-color: blue');
+    //     },
+    //     dblclick: function(){
+    //         $(this).hide();
+    //     }
+    // })
+
+    // $('span.c2').on({
+    //     mouseenter: function(){
+    //         $(this).attr('style',  'background-color: green');
+    //     }, 
+    //     mouseleave: function(){
+    //         $(this).attr('style',  spanC2DefaultColor);
+    //     }
     // })
 }
 
--- Esercitazione 3 JQuery/Esercizio 3/main.js
+++ Esercitazione 3 JQuery/Esercizio 3/main.js
@@ -5,7 +5,9 @@ window.onload = function(){
     $('#btn3').click(caricaDocumento);
     $('#btn4').click(caricaDocumento);
 
+    //VERSIONE JS PLAIN
     function caricaDocumento(e) {
+        // $('#documentID').toggle({height: '75'})
         var httpRequest = new XMLHttpRequest();
         httpRequest.onreadystatechange = gestisciResponse;
         httpRequest.open("GET", e.target.innerHTML+".html", true);
@@ -16,4 +18,9 @@ window.onload = function(){
         document.getElementById("documentID").innerHTML = e.target.responseText;
         }
     }
+
+    //VERSIONE JQUERY (CHE SPETTACOLO!)
+    // function caricaDocumento(e){
+    //     $('#documentID').load(e.target.innerHTML + '.html');
+    // }
 }
